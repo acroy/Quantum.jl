a701579ca8aa97c3cfa2eb868b4668eea1bccf74
Jarrett Revels <jarrettrevels@gmail.com>
2014-06-09 16:11:59 -0400

    added testing for scalar.jl

--- test/scalartest.jl
+++ test/scalartest.jl
@@ -4,8 +4,10 @@ using Quantum
 
 xb = Basis([1:5], "X")
 yb = Basis([1:5], "Y")
-xyb = xb*yb
 
 @test (1+(xb[1]'*yb[1])).ex.args==[:+, 1, InnerProduct(xb[1]', yb[1])]
+for op=(:+, :-, :*, :/)
+	@test @eval qeval((b,k)->b==yb[1]' ? 2.5 : 3.4, ($op)(xb[1]'*yb[1], yb[1]'*xb[1])) == ($op)(3.4,2.5)
+end
 
 end
